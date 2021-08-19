d2bf595156f79a89935ffc6733af6d12ab3b0b0d
mia park <mia1221@kakao.com>
2021-08-19 15:51:29 +0900

    seeRoom

--- src/api/Room/seeRooms/seeRooms.js
+++ src/api/Room/seeRooms/seeRooms.js
@@ -1,18 +1,20 @@
-import { UserInputError } from 'apollo-server';
 import { prisma } from '../../../../generated/prisma-client';
+import { ROOM_FRAGMENT } from '../../../fragment';
 
 export default {
 	Query: {
 		seeRooms: (_, __, { request, isAuthenticated }) => {
 			isAuthenticated(request);
 			const { user } = request;
-			return prisma.rooms({
-				where: {
-					participants_some: {
-						id: UserInputError.id,
+			return prisma
+				.rooms({
+					where: {
+						participants_some: {
+							id: user.id,
+						},
 					},
-				},
-			});
+				})
+				.$fragment(ROOM_FRAGMENT);
 		},
 	},
 };
